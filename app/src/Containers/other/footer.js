6e9c596dfa3637c9a95389d4dc0b63e1137b1eaa
Ewan Questier <ewan.questier@gmail.com>
2023-01-13 13:13:46 +0100

    ewan-nav

--- app/src/Containers/other/footer.js
+++ app/src/Containers/other/footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
     return (
         <div className='footer'>
-            <h1>footer</h1>
+            <p>© 2022 Flatmod</p>
         </div>
     )
 }
